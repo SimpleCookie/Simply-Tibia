46ae550d46eb5d6e569e0dfa46443d4640f4f424
SimpleCookie <Marcus.Ekstrom@devgroup.se>
2024-10-22 19:15:14 +0200

    Stuf

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Navigation } from './Layout/Navigation'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
+      <Navigation />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -18,7 +20,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
