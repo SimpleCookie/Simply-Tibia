444e8d4f6f24896636527289b2414ae490fe2af1
Marcus <Marcus.Ekstrom@devgroup.se>
2024-10-22 16:43:24 +0200

    Initial

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import checker from 'vite-plugin-checker'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+    hmr: {
+      port: 3000,
+    },
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['..'],
+    },
+  },
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+  ],
+  build: {
+    outDir: 'build',
+  },
+})
