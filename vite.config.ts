b708e1371eaadb0a8568c81d46d4ff367542fc1c
Marcus <Marcus.Ekstrom@devgroup.se>
2024-10-23 18:00:51 +0200

    Base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import checker from 'vite-plugin-checker'
 
 export default defineConfig({
-  base: '/',
+  base: 'simply-tibia/',
   server: {
     port: 3000,
     open: true,
